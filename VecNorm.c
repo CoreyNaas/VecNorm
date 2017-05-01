e9a1b7aabdd9f06ab08c4264bc94c633ba519e61
Corey Naas <coreynaas@outlook.com>
2017-05-01 00:01:27 -0500

    stuff

--- VecNorm.c
+++ VecNorm.c
@@ -5,7 +5,7 @@
 //normalize all 10,000 vectors using C and x86 assembly.
 //
 //Special thanks to Brian Scott for collaboration on the x86 assembly function. 50/50 contribution.
-//Special thanks to the people of StackOverflow for helping me fix the little problems and teaching 
+//Special thanks to the people of StackOverflow for helping me fix the little problems and re-teaching 
 //	me the basics of programming as a junior CS student.
 //Special thanks to my mom for keeping me alive those first nine months, and then 21 years after that.
 
@@ -24,7 +24,7 @@ int main(){
 	float a[3];
 	float *vectors;
 	
-	printf( "Enter a vector like so: 3.5 7.3 1.1; or three zeros to generate vectors\n");
+	printf( "Enter a vector like so: 3.5 7.3 1.1; or three zeros to run test\n");
 	scanf("%f %f %f", &a[0], &a[1], &a[2]);
 	
 	//If the vector is zero, generate 10k vectors and run normalization test
@@ -51,6 +51,7 @@ int main(){
 }
 
 int Normalize(float a[]){
+	//establish timekeeping and start timer
 	struct timeval stop, start;
 	gettimeofday(&start, NULL);
 	
@@ -69,6 +70,7 @@ int Normalize(float a[]){
 	
 	printf("Normalized vector by C:[%f %f %f]\n", aN[0], aN[1], aN[2]);
 	
+	//stop timer and print time
 	gettimeofday(&stop, NULL);
 	printf("Operation by C took %dms to complete\n", stop.tv_usec - start.tv_usec);
 	
@@ -77,6 +79,7 @@ int Normalize(float a[]){
 }
 
 int NormalizeASM(float a[]){
+	//establish timekeeping and start timer
 	struct timeval stop, start;
 	gettimeofday(&start, NULL);
 	
@@ -87,16 +90,21 @@ int NormalizeASM(float a[]){
 	
 	//Requirements of the x86 function:
 	//Input: 	An array of three floating point numbers
-	//Output: 	console print of the input vector, but normalized, OR Array of input 
-	//			vector, but normalized, to be printed below
+	//Stuff do:	-Square each vector element
+	//			-sum them together
+	//			-take the square root of the sum = magnitude
+	//			-divide each original element by the magnitude = normalized vector
+	//Output: 	console print of the normalized vector OR normalized vector returned
+	//				to be printed below
 	
 	//Call to x86 assembly vector normalization function
-	//goes 
-	//here 
-	//pl0x
+	//goes
+	//here
+	//plox
 		
 	printf("Normalized vector:[%f %f %f]\n", aN[0], aN[1], aN[2]);
 	
+	//stop timer and print time
 	gettimeofday(&stop, NULL);
 	printf("Operation by x86 took %dms to complete\n", stop.tv_usec - start.tv_usec);
 	
@@ -148,6 +156,7 @@ void Over10kCalculations(float * vec){
 	}
 	
 	//normalize each and every vector using the x86 function. All of them. 
+	//on second thought, it might be better to pass all of the vectors to the function and let the asm function loop, and calculate all of the normalized vectors, and time that, then divide it to find the average. instead of passing them one by one. 
 	j=0;
 	for (int i=0;i<30000;i+=3){
 		temp[0] = vec[i+0];
@@ -164,11 +173,11 @@ void Over10kCalculations(float * vec){
 	totalAvgC = sumTimeC/10000.0;
 	totalAvgASM = sumTimeASM/10000.0;
 
-	printf("The total Calculation time in C is %dms\n", sumTimeC);	
-	printf("The average Calculation time in C is %fms\n", totalAvgC);
+	printf("The total calculation time in C is %dms\n", sumTimeC);	
+	printf("The average calculation time in C is %fms\n", totalAvgC);
 		
-	printf("The total Calculation time in x86 is %dms\n", sumTimeASM);	
-	printf("The average Calculation time in x86 is %fms\n", totalAvgASM);
+	printf("The total calculation time in x86 is %dms\n", sumTimeASM);	
+	printf("The average calculation time in x86 is %fms\n", totalAvgASM);
 	
 	return;
 }
@@ -177,3 +186,4 @@ void Over10kCalculations(float * vec){
 
 //http://stackoverflow.com/questions/10192903/time-in-milliseconds
 //http://stackoverflow.com/questions/13408990/how-to-generate-random-float-number-in-c
+//https://www.3dbuzz.com/forum/threads/104753-HowTo-Inline-Assembly-amp-SSE-Vector-normalization-done-fast!
